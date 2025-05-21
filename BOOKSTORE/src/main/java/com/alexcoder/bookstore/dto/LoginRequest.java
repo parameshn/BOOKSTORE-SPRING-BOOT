e45d4779ae4c4b2bb4f6e35a47eeca6d0717b0be
paramesh <parameshdarshan1234@gmail.com>
2025-05-21 23:47:36 +0530

    LoginRequest

---
+++ BOOKSTORE/src/main/java/com/alexcoder/bookstore/dto/LoginRequest.java
@@ -0,0 +1,16 @@
+package com.alexcoder.bookstore.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+
+@Data
+public class LoginRequest {
+
+    @NotBlank(message = "Username cannot be blank")
+    private String username;
+
+    @NotBlank(message = "Password cannot be blank")
+    private String password;
+
+}
